a3db724d35ffe9dd4950d9e60b5f64d5cafe5f59
Kover CHENG <jueming.cheng@gmail.com>
2023-05-24 17:29:11 +0800

    Add 206 & Update 203 - 0524

--- 203.Remove.Linked.List.Elements/recursion_solution.go
+++ 203.Remove.Linked.List.Elements/recursion_solution.go
@@ -1,10 +1,5 @@
 package main
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
-
 func removeElements(head *ListNode, val int) *ListNode {
 	if head == nil {
 		return nil
---
+++ 206.Reverse.Linked.List/dummy_head_solution.go
@@ -0,0 +1,14 @@
+package main
+
+//func reverseList(head *ListNode) *ListNode {
+//	dummyHead, p := &ListNode{}, head
+//
+//	for p != nil {
+//		q := dummyHead.Next
+//		dummyHead.Next = p
+//		p = p.Next
+//		dummyHead.Next.Next = q
+//	}
+//
+//	return dummyHead.Next
+//}
